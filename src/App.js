0c6b943e30c685cb2fe9cb1359a3f5775dafb548
Alec Dilanchian <alec.dilanchian@gmail.com>
2021-10-14 10:55:41 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
