2e1e969941ecfe272d4855d15fc7f8986a940850
Santi <santiagojimenez.quintero@gmail.com>
2021-11-24 23:43:06 +0100

    Fix warnings when compiling for the first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import * as React from "react";
-import { ethers } from "ethers";
-import './App.css';
+//import { ethers } from "ethers";
+import "./App.css";
 
 export default function App() {
 
   const wave = () => {
-    
+
   }
-  
+
   return (
     <div className="mainContainer">
-
+    
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          <span role="img" aria-label="waving hand">
+            👋
+          </span>{" "}
+          Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
