d142ec6719b2ffd3ab69e0b37dcf1217b18cfda6
wc <wchamberlain89@gmail.com>
2021-12-02 15:37:50 -0800

    Allows users to submit message along with their wave

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useEffect } from "react";
 import './App.css';
+import { ethers } from "ethers";
+import WavePortalABI from './utils/WavePortal.json';
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = React.useState('');
+  const [totalWaves, setTotalWaves] = React.useState();
+  const [allWaves, setAllWaves] = React.useState([]);
+
+  const contractAddress = '0xdf076DFB1fb5599E7A677a53fdB43bb3a0389EB6';
+
+  const contractABI = WavePortalABI.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -24,6 +32,7 @@ const App = () => {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account)
+        getAllWaves();
       } else {
         console.log("No authorized account found")
       }
@@ -47,32 +56,106 @@ const App = () => {
     }
   }
 
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave("this is a message");
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        await getAllWaves();
+
+        count = await wavePortalContract.getTotalWaves();
+        setTotalWaves(count.toNumber())
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if(ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const waves = await wavePortalContract.getAllWaves();
+
+        console.log(waves)
+
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum Object does not exist!");
+      } 
+    } catch(error) {
+        console.error(error);
+      }
+  }
+
   /*
   * This runs our function when the page loads.
   */
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
-  
+
   return (
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Hey there!
         </div>
 
         <div className="bio">
-          {currentAccount}
+          Hey I'm Ward, fresh to Web3 but building fast 🔥🔥🔥.
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
         {!currentAccount && (
           <button className="waveButton" onClick={connectWallet}>
             Connect Wallet
           </button>
         )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
