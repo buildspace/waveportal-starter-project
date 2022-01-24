ba30709df7e598c687123d369fe545c32df1fcd7
WenHan <wenhantan94@gmail.com>
2022-01-24 15:01:38 +0800

    test

---
+++ build/precache-manifest.108354208be6948f79b591d1ba1f94cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0031d85b27f3035c9d98f5333aaaca33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd6ba2251d20a2207cb",
+    "url": "/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "dfd6ba2251d20a2207cb",
+    "url": "/static/js/main.2d8deef3.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
