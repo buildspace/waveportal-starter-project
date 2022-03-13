a6a6a2efcb9cf66ee1184378eeff8e89df7e7ba9
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2022-03-13 11:31:46 +0000

    remove react import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+        <div className="header">👋 Hey there!</div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
