f557ca39f9ee67367af92b47d5d2bba1e4f8cda1
kai-nakao <nkkai0803@gmail.com>
2022-06-27 17:07:19 +0900

    add input function

--- src/App.js
+++ src/App.js
@@ -6,9 +6,47 @@ import abi from './utils/WavePortal.json'
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState('')
 
-  const contractAddress = '0x7E0D4f4EA5C839e23106b1cab79B1872AFFC8a6A'
+  const [inputText, setInputText] = useState('')
+
+  const [allWaves, setAllWaves] = useState([])
+
+  const [currentCount, setCurrentCount] = useState(0)
+
+  const contractAddress = '0xBDe4C77a2E488F6c31c0D7CD914bc9fc0A8C8F86'
 
   const contractABI = abi.abi
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer,
+        )
+
+        const waves = await wavePortalContract.getAllWaves()
+
+        let wavesCleaned = []
+        waves.forEach((wave) => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+          })
+        })
+
+        setAllWaves(wavesCleaned)
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window
@@ -25,6 +63,7 @@ const App = () => {
         const account = accounts[0]
         console.log('Found an authorized account:', account)
         setCurrentAccount(account)
+        getAllWaves()
       } else {
         console.log('No authorized account found')
       }
@@ -46,6 +85,7 @@ const App = () => {
 
       console.log('Connected', accounts[0])
       setCurrentAccount(accounts[0])
+      getAllWaves() //教材にはないが必要と判断して記述
     } catch (error) {
       console.log(error)
     }
@@ -65,6 +105,16 @@ const App = () => {
         )
 
         let count = await wavePortalContract.getTotalWaves()
+        setCurrentCount(count.toNumber())
+        console.log('Retrieved total wave count...', count.toNumber())
+        const waveTxn = await wavePortalContract.wave(inputText)
+        console.log('Mining...', waveTxn.hash)
+
+        await waveTxn.wait()
+        console.log('Mined -- ', waveTxn.hash)
+
+        count = await wavePortalContract.getTotalWaves()
+        setCurrentCount(count.toNumber())
         console.log('Retrieved total wave count...', count.toNumber())
       } else {
         console.log("Ethereum object doesn't exist!")
@@ -76,22 +126,35 @@ const App = () => {
 
   useEffect(() => {
     checkIfWalletIsConnected()
-  })
+  }, [])
 
   return (
     <div className="mainContainer">
       <div className="dataContainer">
-        <div className="header">
-          <span role="img" aria-label="hello">
-            👋 Hey there!
-          </span>
-        </div>
-
+        <div className="header">waveカウントは{currentCount}</div>
         <div className="bio">
           I am farza and I worked on self-driving cars so that's pretty cool
           right? Connect your Ethereum wallet and wave at me!
         </div>
-
+        <form>
+          <label>
+            Message:
+            <input
+              type="text"
+              name="wave"
+              value={inputText}
+              onChange={(event) => setInputText(event.target.value)}
+            />
+          </label>
+          <input
+            type="submit"
+            value="wave"
+            onClick={() => {
+              wave()
+              setInputText('')
+            }}
+          />
+        </form>
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
@@ -100,6 +163,23 @@ const App = () => {
             Connect Wallet
           </button>
         )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                backgroundColor: 'OldLace',
+                marginTop: '16px',
+                padding: '8px',
+              }}
+            >
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
